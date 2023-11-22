e93a93a1fa59febc743d4d3039726fca1116b396
varun pandian <varunpandiandeveloper@gmail.com>
2023-11-22 11:46:21 +0530

    Create basic structure of Sqlalchemy models  database sqllite and framework fastapi

---
+++ README.md
@@ -0,0 +1,29 @@
+# plot-profits-service
+
+
+## Development
+
+### Requirements
+
+* Python >= 3.10
+* pip
+
+
+### Setup
+
+We use virtual environment for development
+
+```sh
+# Create a virtual env
+
+python3 -m venv .venv
+
+# activate virtual env
+
+source .venv/bin/activate
+
+# install dependencies
+
+pip install -r requirements.txt
+
+```
---
+++ main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI,HTTPException
+from utils.dbconnect import SessionLocal, engine
+from models.model import Base, Item
+from pydantic import BaseModel
+
+# Create tables
+Base.metadata.create_all(bind=engine)
+
+# FastAPI app
+app = FastAPI()
+
+#Create Basemodel 
+
+class ItemCreate(BaseModel):
+    name: str
+    description: str
+
+# Route to add a new item
+@app.post("/items/")
+async def add_item(item: ItemCreate):
+    db = SessionLocal()
+    try:
+        new_item = Item(name=item.name, description=item.description)
+        db.add(new_item)
+        db.commit()
+        db.refresh(new_item)
+        return new_item
+    finally:
+        db.close()
+
+# Route to get an item by ID
+@app.get("/items/{item_id}")
+async def get_item(item_id: int):
+    db = SessionLocal()
+    try:
+        item = db.query(Item).filter(Item.id == item_id).first()
+        if item is None:
+            raise HTTPException(status_code=404, detail="Item not found")
+        return {"id": item.id, "name": item.name, "description": item.description}
+    finally:
+        db.close()
